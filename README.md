86cb50f74e2327427435986584379364b0b3af12
Guillaume Gérard <guillaume.gerard88@gmail.com>
2021-12-08 22:37:08 +0100

    feat: setup first version

---
+++ README.md
@@ -0,0 +1,70 @@
+# Gist🔗🌲
+
+[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
+[![Test](https://github.com/GreatWizard/gistlinktree/actions/workflows/test.yml/badge.svg)](https://github.com/GreatWizard/gistlinktree/actions/workflows/test.yml)
+
+Generate a link tree from a configuration file.
+
+## Installation
+
+```shell
+npm install --save gistlinktree
+```
+
+## Usage
+
+```shell
+npx gistlinktree [options]
+```
+
+### Options
+
+```shell
+  -c, --config string       Path to the config file
+  -o, --output-dir string   Output directory
+  -h, --help                Print this usage guide
+```
+
+Config file is defaulted to `gistlinktree.yml`.
+
+Output directory is defaulted to `dist`.
+
+## Configuration
+
+Define a `gistlinktree.yml` file as following:
+
+```yaml
+title: My Personal Link Tree # Main title of your page
+avatar: static/avatar.jpg # avatar used in priority over gravatar
+gravatar: <md5-hash-of-your-email-here> # load avatar from gravatar
+theme: dracula # theme of the stylesheet; values: default (by default), dracula
+index:
+  copy: # copy file at the root directory without changing the name
+    - input: static/icons/favicon.ico
+    - input: static/CNAME
+links:
+  - outputDir: my-tiny-static-website # build the gist into a subfolder
+    gistID: <id-of-the-gist-here> # you need to specify the if of your gist
+    copy:
+      - input: static/icons/website.ico # file to copy
+        output: favicon.ico # rename the file
+  - title: Github.com # title of the button link
+    url: https://github.com # define the URL of the link
+linksSocialPosition: bottom # position of the social links in the page; values: bottom (by default), top
+linksSocial:
+  - type: github # icon of the social link (use Font Awesome solid and brand icon names)
+    url: https://github.com/<my-nickname> # define the URL of the social link
+```
+
+ℹ️ SASS/SCSS/CSS files added in the `index.copy` list are added automatically into the generated `index.html`.
+
+### Format conversion
+
+During the copy process or the build process of a gist, it converts:
+
+- markdown files into HTML
+- SASS/SCSS files into CSS
+
+### Social Links
+
+Only solid and brand icons coming from Font Awesome v5.15 are available: https://fontawesome.com/v5.15/icons?d=gallery&p=2&s=brands,solid&m=free
---
+++ index.js
@@ -0,0 +1,169 @@
+#!/usr/bin/env node
+import fs from 'fs'
+import fsPromises from 'fs/promises'
+import { dirname, basename, join } from 'path'
+import YAML from 'yaml'
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
+import { fileURLToPath } from 'url'
+
+import { getGist } from './lib/gist.js'
+import { determineFormat, writeFile } from './lib/files.js'
+import { generateIndex } from './lib/html.js'
+import { configSchema } from './lib/schema.js'
+
+const optionList = [
+  {
+    name: 'config',
+    alias: 'c',
+    type: String,
+    defaultOption: true,
+    defaultValue: 'gistlinktree.yml',
+    description: 'Path to the config file',
+  },
+  {
+    name: 'output-dir',
+    alias: 'o',
+    type: String,
+    defaultValue: 'dist',
+    description: 'Output directory',
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Print this usage guide',
+  },
+]
+
+const options = commandLineArgs(optionList)
+
+if (options.help) {
+  console.log(
+    commandLineUsage([
+      {
+        content: `Gist🔗${new Date().getMonth() == 11 ? '🎄' : '🌲'}`,
+      },
+      {
+        header: 'Options',
+        optionList,
+      },
+    ]),
+  )
+} else {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = dirname(__filename)
+  const cwd = await dirname(options.config)
+
+  const index = []
+  const styleSheets = []
+
+  const config = YAML.parse(await fsPromises.readFile(options.config, 'utf8'))
+  let { error } = configSchema.validate(config, { abortEarly: false })
+  if (error) {
+    throw new Error(`Configuration file is invalid:${error.details.map((detail) => `\n  - ${detail.message}`)}`)
+  }
+
+  if (!fs.existsSync(options['output-dir'])) {
+    await fsPromises.mkdir(options['output-dir'], { recursive: true })
+  }
+
+  let theme = config?.theme
+
+  if (!theme || !fs.existsSync(join(__dirname, 'themes', `${theme}.scss`))) {
+    theme = 'default'
+  }
+
+  const styleFilename = basename(
+    await writeFile(join(options['output-dir'], 'style.css'), 'scss', {
+      file: join(__dirname, 'themes', `${theme}.scss`),
+      fingerprint: true,
+      includePaths: [
+        join(__dirname, 'node_modules', 'normalize.css'),
+        join(__dirname, 'node_modules', 'milligram', 'src'),
+        join(cwd, 'node_modules', 'normalize.css'),
+        join(cwd, 'node_modules', 'milligram', 'src'),
+      ],
+    }),
+  )
+
+  let avatarFilename = undefined
+  if (config?.avatar) {
+    const avatarFormat = determineFormat(config.avatar)
+    avatarFilename = basename(
+      await writeFile(join(options['output-dir'], `avatar.${avatarFormat}`), avatarFormat, {
+        file: config.avatar,
+        fingerprint: true,
+      }),
+    )
+  }
+
+  if (config?.index?.copy) {
+    for (let copy of config.index.copy || []) {
+      let format = determineFormat(copy.input)
+      let outputFilename = copy.output || basename(copy.input)
+      let isStyleSheet = format === 'sass' || format === 'scss' || format === 'css'
+      let newFile = await writeFile(join(options['output-dir'], outputFilename), format, {
+        file: join(cwd, copy.input),
+        fingerprint: isStyleSheet,
+      })
+      if (isStyleSheet) {
+        styleSheets.push(basename(newFile))
+      }
+    }
+  }
+
+  for (let linkConfig of config?.links || []) {
+    if (linkConfig.outputDir) {
+      let outputDir = join(options['output-dir'], linkConfig.outputDir)
+      if (!fs.existsSync(outputDir)) {
+        await fsPromises.mkdir(outputDir, { recursive: true })
+      }
+
+      for (let copy of linkConfig.copy || []) {
+        await writeFile(join(outputDir, copy.output || basename(copy.input)), determineFormat(copy.input), {
+          file: join(cwd, copy.input),
+        })
+      }
+
+      if (linkConfig.gistID) {
+        let parsed = await getGist(linkConfig.gistID)
+        let options = {
+          mainTitle: config?.title,
+          title: parsed.description,
+          favicon:
+            parsed.files['favicon.ico'] !== undefined ||
+            linkConfig.copy?.find((f) => basename(f.input) === 'favicon.ico' || f.output === 'favicon.ico'),
+          mainStyleSheet: styleFilename,
+        }
+        let files = Object.values(parsed.files)
+        for (let file of files) {
+          await writeFile(join(outputDir, file.filename), determineFormat(file.filename), {
+            data: file['content'],
+            ...options,
+          })
+        }
+        index.push({
+          title: parsed.description,
+          url: linkConfig.outputDir,
+        })
+      }
+    }
+
+    if (linkConfig.url && linkConfig.title) {
+      index.push({ title: linkConfig.title, url: linkConfig.url })
+    }
+  }
+
+  const indexContent = await generateIndex(index, config.linksSocial, {
+    mainTitle: config?.title,
+    avatar: avatarFilename,
+    gravatar: config?.gravatar,
+    favicon: config?.index?.copy?.find((f) => basename(f.input) === 'favicon.ico' || f.output === 'favicon.ico'),
+    mainStyleSheet: styleFilename,
+    styleSheets,
+    linksSocialPosition: config?.linksSocialPosition,
+  })
+
+  await fsPromises.writeFile(join(options['output-dir'], 'index.html'), indexContent)
+}
